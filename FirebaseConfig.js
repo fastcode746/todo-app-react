0e79f3f1096f4f50e816c76ff609988fca61a8b8
ZARGHAM CH <121686435+zargham746@users.noreply.github.com>
2024-07-31 11:59:49 +0500

    updates

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuTTkMEuo9lbw8_EEn_iQ3R9jMLlYfuDU",
-  authDomain: "mycrudshopsbook.firebaseapp.com",
-  projectId: "mycrudshopsbook",
-  storageBucket: "mycrudshopsbook.appspot.com",
-  messagingSenderId: "922762553677",
-  appId: "1:922762553677:web:256fae3afb39425ef9ef04",
-  measurementId: "G-ZRMQHML7GQ",
+  apiKey: "AIzaSyANOkXrwMl3tJ6wtsRmt-7ybh0qNZGogDU",
+  authDomain: "my-todo-app-6d9ec.firebaseapp.com",
+  projectId: "my-todo-app-6d9ec",
+  storageBucket: "my-todo-app-6d9ec.appspot.com",
+  messagingSenderId: "338072743901",
+  appId: "1:338072743901:web:eb41c34ab92ddc7b571034",
+  measurementId: "G-PHRR43WSZ2"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
